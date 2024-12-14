bbc2e675134b6f62d9e61393cd70ba9ce5bc52bf
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-12-14 13:33:18 +0700

    refactor(api): encapsulate web request in repository FlickrFetcher

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrFetcher.kt
@@ -0,0 +1,43 @@
+package com.bignerdranch.android.photogallery.api
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "FlickrFetcher"
+
+class FlickrFetcher {
+
+    private val flickrApi: FlickrApi
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+        flickrApi = retrofit.create(FlickrApi::class.java)
+    }
+
+    fun fetchContent(): LiveData<String> {
+        val responseLiveData: MutableLiveData<String> = MutableLiveData()
+        val flickrRequest: Call<String> = flickrApi.fetchContent()
+        flickrRequest.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+            override fun onResponse(
+                call: Call<String>,
+                response: Response<String>
+            ) {
+                Log.d(TAG, "Response received")
+                responseLiveData.value = response.body()
+            }
+        })
+        return responseLiveData
+    }
+}
